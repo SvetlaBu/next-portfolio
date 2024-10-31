4ef69866229a55f3766b681e59a98c76e6d74904
SvetlaBu <85964900+SvetlaBu@users.noreply.github.com>
2024-10-31 18:57:52 +0300

    Update Sound.jsx

--- src/components/Sound.jsx
+++ src/components/Sound.jsx
@@ -87,7 +87,7 @@ const Sound = () => {
       )}
 
       <audio ref={audioRef} loop>
-        <source src={"/next-portfolio/public/audio/birds39-forest-20772.mp3"} type="audio/mpeg" />
+        <source src={"${basePath}/public/audio/birds39-forest-20772.mp3"} type="audio/mpeg" />
         your browser does not support the audio element.
       </audio>
       <motion.button
