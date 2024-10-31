dc47f176b505dabc133e9e6d52fb5da4db521e5a
SvetlaBu <85964900+SvetlaBu@users.noreply.github.com>
2024-11-01 01:31:49 +0300

    Update and rename next.config.js to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
     distDir: 'dist',
     images: {
         unoptimized: true,
-    }
+    },
     // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
     // trailingSlash: true,
    
