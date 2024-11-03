0f5932cfa70edcc88f596da2c8dba0438c5815fa
SvetlaBu <85964900+SvetlaBu@users.noreply.github.com>
2024-11-04 02:12:32 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,73 +2,73 @@
 
 // const isProd = process.env.NODE_ENV = 'production';
 
-// const nextConfig = {
-//     output: 'export',
-//     distDir: 'dist',
-//     images: {
-//         unoptimized: true,
-//     },
-//     // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-//     // trailingSlash: true,
-   
-//     // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-//     // skipTrailingSlashRedirect: true,
-   
-//     // Optional: Change the output directory `out` -> `dist`
-//     // distDir: 'dist',
-// }
-   
-//   module.exports = nextConfig
-
 const nextConfig = {
     output: 'export',
     distDir: 'dist',
-    webpack(config) {
-        const fileLoaderRule = config.module.rules.find((rule) =>
-            rule.test?.test?.('.svg'),
-        )
-        config.module.rules.push(
-            {
-            ...fileLoaderRule,
-            test: /\.svg$/i,
-            resourceQuery: /url/, // *.svg?url
-            },
-            {
-                test: /\.svg$/i,
-                issuer: fileLoaderRule.issuer,
-                resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
-                use: [{ loader: '@svgr/webpack', options: { icon: true } }]
-            },
-            )
-            fileLoaderRule.exclude = /\.svg$/i
-            return config
-        },
     images: {
         unoptimized: true,
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'flagcdn.com',
-                port: '',
-            },
-        ],
     },
-    // module.exports: {
-    //     test: /\.(gif|png|jpg|jpeg|mp3|glb)?$/i,
-    //     type: 'asset/resource',
-    //     },
-    // },
-   module: {
-     rules: [
-      {
-        test: /\.(gif|png|jpg|jpeg|mp3|glb)?$/i,
-        type: 'asset/resource',
-      },
-     ],
-   },
-    compiler: {
-        styledComponents: true
-    },
-};
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    // trailingSlash: true,
+   
+    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+    // skipTrailingSlashRedirect: true,
+   
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
+}
+   
+  module.exports = nextConfig
 
-export default nextConfig;
+// const nextConfig = {
+//     output: 'export',
+//     distDir: 'dist',
+//     webpack(config) {
+//         const fileLoaderRule = config.module.rules.find((rule) =>
+//             rule.test?.test?.('.svg'),
+//         )
+//         config.module.rules.push(
+//             {
+//             ...fileLoaderRule,
+//             test: /\.svg$/i,
+//             resourceQuery: /url/, // *.svg?url
+//             },
+//             {
+//                 test: /\.svg$/i,
+//                 issuer: fileLoaderRule.issuer,
+//                 resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+//                 use: [{ loader: '@svgr/webpack', options: { icon: true } }]
+//             },
+//             )
+//             fileLoaderRule.exclude = /\.svg$/i
+//             return config
+//         },
+//     images: {
+//         unoptimized: true,
+//         remotePatterns: [
+//             {
+//                 protocol: 'https',
+//                 hostname: 'flagcdn.com',
+//                 port: '',
+//             },
+//         ],
+//     },
+//     module.exports: {
+//         test: /\.(gif|png|jpg|jpeg|mp3|glb)?$/i,
+//         type: 'asset/resource',
+//         },
+//     },
+//    module: {
+//      rules: [
+//       {
+//         test: /\.(gif|png|jpg|jpeg|mp3|glb)?$/i,
+//         type: 'asset/resource',
+//       },
+//      ],
+//    },
+//     compiler: {
+//         styledComponents: true
+//     },
+// };
+
+// export default nextConfig;
