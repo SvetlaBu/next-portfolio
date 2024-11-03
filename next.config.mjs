897841cdc99c6209995fe85413928bb6410361ac
SvetlaBu <85964900+SvetlaBu@users.noreply.github.com>
2024-11-03 20:36:53 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,69 @@
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
+
+// const isProd = process.env.NODE_ENV = 'production';
+
+// const nextConfig = {
+//     output: 'export',
+//     distDir: 'dist',
+//     images: {
+//         unoptimized: true,
+//     },
+//     // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+//     // trailingSlash: true,
+   
+//     // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+//     // skipTrailingSlashRedirect: true,
+   
+//     // Optional: Change the output directory `out` -> `dist`
+//     // distDir: 'dist',
+// }
+   
+//   module.exports = nextConfig
+
 const nextConfig = {
+    // basePath: isProd ? '/intern-pognali-2-4' : '',
     output: 'export',
     distDir: 'dist',
+    webpack(config) {
+        const fileLoaderRule = config.module.rules.find((rule) =>
+            rule.test?.test?.('.svg'),
+        )
+        config.module.rules.push(
+            {
+            ...fileLoaderRule,
+            test: /\.svg$/i,
+            resourceQuery: /url/, // *.svg?url
+            },
+            {
+                test: /\.svg$/i,
+                issuer: fileLoaderRule.issuer,
+                resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+                use: [{ loader: '@svgr/webpack', options: { icon: true } }]
+            },
+            )
+            fileLoaderRule.exclude = /\.svg$/i
+            return config
+        },
     images: {
         unoptimized: true,
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'flagcdn.com',
+                port: '',
+            },
+        ],
     },
-    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-    // trailingSlash: true,
-   
-    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-    // skipTrailingSlashRedirect: true,
-   
-    // Optional: Change the output directory `out` -> `dist`
-    // distDir: 'dist',
-    webpack(config) {
-        .addLoader({
-        test: /\.(glb|gltf|mp3)$/,
-        loader: 'file-loader'
-        })
-    }
-}
-   
-  module.exports = nextConfig
+    exports: {
+        test: /\.(gif|png|jpg|jpeg|mp3|glb))?$/,
+        loader: 'file-loader',
+        options: {
+        name: 'assets/img/[name].[ext]',
+        },
+    },
+    compiler: {
+        styledComponents: true
+    },
+};
+
+export default nextConfig;
