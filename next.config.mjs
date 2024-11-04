d4e2ee2cc03c3a43b2865ab28ea1c7d0022dd9c7
SvetlaBu <85964900+SvetlaBu@users.noreply.github.com>
2024-11-04 12:32:30 +0300

    fix: search path

--- next.config.mjs
+++ next.config.mjs
@@ -2,67 +2,29 @@
 
 // const isProd = process.env.NODE_ENV = 'production';
 
-// const nextConfig = {
-//     output: 'export',
-//     distDir: 'dist',
-//     images: {
-//         unoptimized: true,
-//     },
-//     // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-//     // trailingSlash: true,
-   
-//     // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-//     // skipTrailingSlashRedirect: true,
-   
-//     // Optional: Change the output directory `out` -> `dist`
-//     // distDir: 'dist',
-// }
-   
-//   module.exports = nextConfig
-
 const nextConfig = {
     output: 'export',
     distDir: 'dist',
-    webpack(config) {
-        const fileLoaderRule = config.module.rules.find((rule) =>
-            rule.test?.test?.('.svg'),
-        )
-        config.module.rules.push(
-            {
-            ...fileLoaderRule,
-            test: /\.svg$/i,
-            resourceQuery: /url/, // *.svg?url
-            },
-            {
-                test: /\.svg$/i,
-                issuer: fileLoaderRule.issuer,
-                resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
-                use: [{ loader: '@svgr/webpack', options: { icon: true } }]
-            },
-            )
-            fileLoaderRule.exclude = /\.svg$/i
-            return config
-        },
     images: {
         unoptimized: true,
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'flagcdn.com',
-                port: '',
-            },
-        ],
     },
-    exports: {
-        test: /\.(gif|png|jpg|jpeg|mp3$|glb)?/,
-        loader: 'file-loader',
-        options: {
-        name: 'assets/[name].[ext]',
-        },
-    },
-    compiler: {
-        styledComponents: true
-    },
-};
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    // trailingSlash: true,
+   
+    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+    // skipTrailingSlashRedirect: true,
+   
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
+}
+   
+//   module.exports = nextConfig
+// const assetHost = process.env.ASSET_HOST;
+
+// const nextConfig = {
+//     output: 'export',
+//     distDir: 'dist',
+//     assetPrefix: isPresent(assetHost) ? assetHost : undefined,
+// };
 
 export default nextConfig;
